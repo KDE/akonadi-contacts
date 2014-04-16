4c6f51c0cc1245e2145771b3583a0fba9009e9ef
Montel Laurent <montel@kde.org>
2014-04-16 19:56:08 +0200

    Move to toplevel

---
+++ akonadi-contact/Messages.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+$EXTRACTRC src/*.ui src/actions/*.kcfg.cmake src/*.kcfg >> rc.cpp || exit 11
+$XGETTEXT src/*.cpp src/*.h src/actions/*.cpp src/editor/*.cpp src/editor/im/*.cpp -o $podir/akonadicontact.pot
+rm -f rc.cpp
