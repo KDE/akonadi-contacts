6dc99acb3f12c3bf4e1e92800acd653ab2cd8895
Montel Laurent <montel@kde.org>
2014-06-01 11:11:28 +0200

    Change message filename to allow to co-install kde4/kf5 if we want

--- akonadi-contact/Messages.sh
+++ akonadi-contact/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC src/*.ui src/actions/*.kcfg.cmake src/*.kcfg >> rc.cpp || exit 11
-$XGETTEXT src/*.cpp src/*.h src/actions/*.cpp src/editor/*.cpp src/editor/im/*.cpp -o $podir/akonadicontact.pot
+$XGETTEXT src/*.cpp src/*.h src/actions/*.cpp src/editor/*.cpp src/editor/im/*.cpp -o $podir/akonadicontact5.pot
 rm -f rc.cpp
