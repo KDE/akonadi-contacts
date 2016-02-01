d05b7bba2a253f15bf91001cae735fdc3b0c13e2
Montel Laurent <montel@kde.org>
2016-02-01 21:21:26 +0100

    start to redesign addresseditor

---
+++ akonadi-contact/src/editor/addresseditor/addresslocationwidget.h
@@ -0,0 +1,45 @@
+/*
+    This file is part of Akonadi Contact.
+
+    Copyright (C) 2016 eyeOS S.L.U., a Telefonica company, sales@eyeos.com
+    Copyright (C) 2016 Laurent Montel <laurent.montel@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+
+#ifndef ADDRESSLOCATIONWIDGET_H
+#define ADDRESSLOCATIONWIDGET_H
+
+#include <QWidget>
+#include <KContacts/Address>
+class QCheckBox;
+
+class AddressLocationWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AddressLocationWidget(QWidget *parent = Q_NULLPTR);
+    ~AddressLocationWidget();
+
+    void setAddress(const KContacts::Address &address);
+    KContacts::Address address() const;
+
+private:
+    QCheckBox *mPreferredCheckBox;
+};
+
+#endif // ADDRESSLOCATIONWIDGET_H
