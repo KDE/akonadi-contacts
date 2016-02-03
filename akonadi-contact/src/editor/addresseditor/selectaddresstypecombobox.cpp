11f75f0037577c9da442e11d5bd7c2e212bda3b4
Montel Laurent <montel@kde.org>
2016-02-03 07:05:38 +0100

    Add combobox

---
+++ akonadi-contact/src/editor/addresseditor/selectaddresstypecombobox.cpp
@@ -0,0 +1,108 @@
+/*
+    This file is part of Akonadi Contact.
+
+    Copyright (C) 2016 eyeOS S.L.U., a Telefonica company, sales@eyeos.com
+    Copyright (C) 2016 Laurent Montel <laurent.montel@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+
+#include "selectaddresstypecombobox.h"
+#include <KLocalizedString>
+
+SelectAddressTypeComboBox::SelectAddressTypeComboBox(QWidget *parent)
+    : KComboBox(parent)
+    , mType(KContacts::Address::Home)
+    , mLastSelected(0)
+{
+    const int count = KContacts::Address::typeList().count();
+    mTypeList.reserve(count + 1);
+    for (int i = 0; i < count; ++i) {
+        mTypeList.append(KContacts::Address::typeList().at(i));
+    }
+    mTypeList.append(-1);   // Others...
+
+    update();
+
+    connect(this, SIGNAL(activated(int)),
+            this, SLOT(selected(int)));
+}
+
+SelectAddressTypeComboBox::~SelectAddressTypeComboBox()
+{
+}
+
+void SelectAddressTypeComboBox::setType(KContacts::Address::Type type)
+{
+    if (!mTypeList.contains((int)type)) {
+        // insert at the end, but before the 'Others...' entry
+        mTypeList.insert(mTypeList.at(mTypeList.count() - 1), (int)type);
+    }
+
+    mType = type;
+    update();
+}
+
+KContacts::Address::Type SelectAddressTypeComboBox::type() const
+{
+    return mType;
+}
+
+void SelectAddressTypeComboBox::update()
+{
+    bool blocked = signalsBlocked();
+    blockSignals(true);
+
+    clear();
+    for (int i = 0; i < mTypeList.count(); ++i) {
+        if (mTypeList.at(i) == -1) {     // "Other..." entry
+            addItem(i18nc("@item:inlistbox Category of contact info field", "Other..."));
+        } else {
+            addItem(KContacts::Address::typeLabel(KContacts::Address::Type(mTypeList.at(i))));
+        }
+    }
+
+    setCurrentIndex(mLastSelected = mTypeList.indexOf(mType));
+
+    blockSignals(blocked);
+}
+
+void SelectAddressTypeComboBox::selected(int pos)
+{
+    if (mTypeList.at(pos) == -1) {
+        otherSelected();
+    } else {
+        mType = KContacts::Address::Type(mTypeList.at(pos));
+        mLastSelected = pos;
+    }
+}
+
+void SelectAddressTypeComboBox::otherSelected()
+{
+#if 0
+    AutoQPointer<AddressTypeDialog> dlg = new AddressTypeDialog(mType, this);
+    if (dlg->exec()) {
+        mType = dlg->type();
+        if (!mTypeList.contains(mType)) {
+            mTypeList.insert(mTypeList.at(mTypeList.count() - 1), mType);
+        }
+    } else {
+        setType(KContacts::Address::Type(mTypeList.at(mLastSelected)));
+    }
+#endif
+    update();
+}
