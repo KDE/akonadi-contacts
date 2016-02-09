53e0822e29ec55195ad38c8afe57fb22a3cdf618
Montel Laurent <montel@kde.org>
2016-02-09 14:54:08 +0100

    Add combobox to select type of phone

---
+++ akonadi-contact/src/editor/generalinfoeditor/phone/phonecomboboxtype.cpp
@@ -0,0 +1,107 @@
+/*
+    This file is part of Akonadi Contact.
+
+    Copyright (C) 2016 eyeOS S.L.U., a Telefonica company, sales@eyeos.com
+    Copyright (C) 2016 Laurent Montel <laurent.montel@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "phonecomboboxtype.h"
+#include <KLocalizedString>
+
+using namespace Akonadi;
+
+PhoneComboBoxType::PhoneComboBoxType(QWidget *parent)
+    : KComboBox(parent)
+    , mType(KContacts::PhoneNumber::Home)
+    , mLastSelected(0)
+{
+    const int nbMax = KContacts::PhoneNumber::typeList().count();
+    mTypeList.reserve(nbMax + 1);
+    for (int i = 0; i < nbMax; ++i) {
+        mTypeList.append(KContacts::PhoneNumber::typeList().at(i));
+    }
+
+    mTypeList.append(-1);   // Others...
+
+    update();
+
+    connect(this, static_cast<void (PhoneComboBoxType::*)(int)>(&PhoneComboBoxType::activated), this, &PhoneComboBoxType::selected);
+}
+
+PhoneComboBoxType::~PhoneComboBoxType()
+{
+}
+
+void PhoneComboBoxType::setType(KContacts::PhoneNumber::Type type)
+{
+    if (!mTypeList.contains(type)) {
+        mTypeList.insert(mTypeList.at(mTypeList.count() - 1), type);
+    }
+
+    mType = type;
+    update();
+}
+
+KContacts::PhoneNumber::Type PhoneComboBoxType::type() const
+{
+    return mType;
+}
+
+void PhoneComboBoxType::update()
+{
+    clear();
+
+    const int typeListCount(mTypeList.count());
+    for (int i = 0; i < typeListCount; ++i) {
+        if (mTypeList.at(i) == -1) {     // "Other..." entry
+            addItem(i18nc("@item:inlistbox Category of contact info field", "Other..."));
+        } else {
+            addItem(KContacts::PhoneNumber::typeLabel(KContacts::PhoneNumber::Type(mTypeList.at(i))));
+        }
+    }
+
+    setCurrentIndex(mLastSelected = mTypeList.indexOf(mType));
+}
+
+void PhoneComboBoxType::selected(int pos)
+{
+    if (mTypeList.at(pos) == -1) {
+        otherSelected();
+    } else {
+        mType = KContacts::PhoneNumber::Type(mTypeList.at(pos));
+        mLastSelected = pos;
+    }
+}
+
+void PhoneComboBoxType::otherSelected()
+{
+    //TODO
+#if 0
+    AutoQPointer<PhoneTypeDialog> dlg = new PhoneTypeDialog(mType, this);
+    if (dlg->exec()) {
+        mType = dlg->type();
+        if (!mTypeList.contains(mType)) {
+            mTypeList.insert(mTypeList.at(mTypeList.count() - 1), mType);
+        }
+    } else {
+        setType(KContacts::PhoneNumber::Type(mTypeList.at(mLastSelected)));
+    }
+
+    update();
+#endif
+}
---
+++ akonadi-contact/src/editor/generalinfoeditor/phone/phonecomboboxtype.h
@@ -0,0 +1,72 @@
+/*
+    This file is part of Akonadi Contact.
+
+    Copyright (C) 2016 eyeOS S.L.U., a Telefonica company, sales@eyeos.com
+    Copyright (C) 2016 Laurent Montel <laurent.montel@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef PHONECOMBOBOXTYPE_H
+#define PHONECOMBOBOXTYPE_H
+
+#include <KComboBox>
+#include <kcontacts/addressee.h>
+
+namespace Akonadi
+{
+/**
+ * @short A combobox to select a phone number type.
+ */
+class PhoneComboBoxType : public KComboBox
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Creates a phone type combo.
+     *
+     * @param parent The parent widget.
+     */
+    explicit PhoneComboBoxType(QWidget *parent = Q_NULLPTR);
+
+    /**
+     * Destroys the phone type combo.
+     */
+    ~PhoneComboBoxType();
+
+    /**
+     * Sets the phone number @p type that shall be selected.
+     */
+    void setType(KContacts::PhoneNumber::Type type);
+
+    /**
+     * Returns the selected phone number type.
+     */
+    KContacts::PhoneNumber::Type type() const;
+
+private Q_SLOTS:
+    void selected(int);
+    void otherSelected();
+
+private:
+    void update();
+    KContacts::PhoneNumber::Type mType;
+    int mLastSelected;
+    QList<int> mTypeList;
+};
+}
+#endif
