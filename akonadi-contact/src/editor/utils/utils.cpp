6c250dc28a4b769d2dbdfa009fde534538b32d2d
Montel Laurent <montel@kde.org>
2016-02-12 13:18:23 +0100

    Add utils class

---
+++ akonadi-contact/src/editor/utils/utils.cpp
@@ -0,0 +1,38 @@
+/*
+    This file is part of Akonadi Contact.
+
+    Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+
+#include "utils.h"
+
+void Akonadi::Utils::splitCustomField(const QString &str, QString &app, QString &name, QString &value)
+{
+    const int colon = str.indexOf(QLatin1Char(':'));
+    if (colon != -1) {
+        const QString tmp = str.left(colon);
+        value = str.mid(colon + 1);
+
+        const int dash = tmp.indexOf(QLatin1Char('-'));
+        if (dash != -1) {
+            app = tmp.left(dash);
+            name = tmp.mid(dash + 1);
+        }
+    }
+}
---
+++ akonadi-contact/src/editor/utils/utils.h
@@ -0,0 +1,34 @@
+/*
+    This file is part of Akonadi Contact.
+
+    Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QString>
+
+namespace Akonadi
+{
+namespace Utils
+{
+void splitCustomField(const QString &str, QString &app, QString &name, QString &value);
+}
+}
+#endif // UTILS_H
