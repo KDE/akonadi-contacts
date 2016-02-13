e54346ac5e954eb014f2e8501562a8b8ab6f653c
Montel Laurent <montel@kde.org>
2016-02-13 15:12:56 +0100

    Use new design

--- akonadi-contact/src/editor/contacteditorwidget.cpp
+++ akonadi-contact/src/editor/contacteditorwidget.cpp
@@ -50,10 +50,10 @@
 #include <QCheckBox>
 #include <QVBoxLayout>
 #include <KCompletion/KLineEdit>
-#include <editor/addresseditor/addresseslocationwidget.h>
+#include "addresseditor/addresseslocationwidget.h"
 #include "customfieldeditor/customfieldswidget.h"
-
-#include <editor/businesseditor/businesseditorwidget.h>
+#include "businesseditor/businesseditorwidget.h"
+#include "generalinfoeditor/generalinfowidget.h"
 
 class Q_DECL_HIDDEN ContactEditorWidget::Private
 {
@@ -61,7 +61,7 @@ public:
     Private(ContactEditorWidget::DisplayMode displayMode, ContactEditorWidget *parent)
         : mDisplayMode(displayMode)
         , mParent(parent)
-        , mCustomFieldsWidget(0)
+        , mCustomFieldsWidget(Q_NULLPTR)
     {
     }
 
@@ -82,25 +82,8 @@ public:
     ContactEditorWidget *mParent;
     QTabWidget *mTabWidget;
 
-    // widgets from name group
-    NameEditWidget *mNameWidget;
-    ImageWidget *mPhotoWidget;
-    DisplayNameEditWidget *mDisplayNameWidget;
-    KLineEdit *mNickNameWidget;
+    Akonadi::GeneralInfoWidget *mGeneralInfoWidget;
 
-    // widgets from Internet group
-    EmailEditWidget *mEmailWidget;
-    KLineEdit *mHomepageWidget;
-    KLineEdit *mBlogWidget;
-    IMEditWidget *mIMWidget;
-
-    // widgets from phones group
-    PhoneEditWidget *mPhonesWidget;
-
-    CategoriesEditWidget *mCategoriesWidget;
-
-    KComboBox *mMailPreferFormatting;
-    QCheckBox *mAllowRemoteContent;
 
     // widgets from addresses group
     Akonadi::AddressesLocationWidget *mAddressesLocationWidget;
@@ -140,133 +123,8 @@ void ContactEditorWidget::Private::initGui()
 
 void ContactEditorWidget::Private::initGuiContactTab()
 {
-    QWidget *widget = new QWidget;
-    QGridLayout *layout = new QGridLayout(widget);
-
-    mTabWidget->addTab(widget, i18nc("@title:tab", "Contact"));
-
-    QGroupBox *nameGroupBox = new QGroupBox(i18nc("@title:group Name related properties of a contact", "Name"));
-    QGroupBox *internetGroupBox = new QGroupBox(i18nc("@title:group", "Internet"));
-    QGroupBox *phonesGroupBox = new QGroupBox(i18nc("@title:group", "Phones"));
-
-    nameGroupBox->setMinimumSize(320, 200);
-    layout->addWidget(nameGroupBox, 0, 0);
-    layout->addWidget(internetGroupBox, 0, 1);
-    layout->addWidget(phonesGroupBox, 1, 0, 4, 1);
-
-    QGridLayout *nameLayout = new QGridLayout(nameGroupBox);
-    QGridLayout *internetLayout = new QGridLayout(internetGroupBox);
-    QGridLayout *phonesLayout = new QGridLayout(phonesGroupBox);
-
-    // setup name group box
-    QLabel *label = new QLabel(i18nc("@label The name of a contact", "Name:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    nameLayout->addWidget(label, 0, 0);
-
-    mNameWidget = new NameEditWidget;
-    label->setBuddy(mNameWidget);
-    nameLayout->addWidget(mNameWidget, 0, 1);
-
-    mPhotoWidget = new ImageWidget(ImageWidget::Photo);
-    nameLayout->addWidget(mPhotoWidget, 0, 2, 4, 1);
-
-    label = new QLabel(i18nc("@label The display name of a contact", "Display:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    nameLayout->addWidget(label, 1, 0);
-
-    mDisplayNameWidget = new DisplayNameEditWidget;
-    label->setBuddy(mDisplayNameWidget);
-    nameLayout->addWidget(mDisplayNameWidget, 1, 1);
-
-    label = new QLabel(i18nc("@label The nickname of a contact", "Nickname:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    nameLayout->addWidget(label, 2, 0);
-
-    mNickNameWidget = new KLineEdit;
-    mNickNameWidget->setTrapReturnKey(true);
-    label->setBuddy(mNickNameWidget);
-    nameLayout->addWidget(mNickNameWidget, 2, 1);
-
-    nameLayout->setRowStretch(3, 1);
-
-    // setup Internet group box
-    label = new QLabel(i18nc("@label The email address of a contact", "Email:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    internetLayout->addWidget(label, 0, 0);
-
-    mEmailWidget = new EmailEditWidget;
-    label->setBuddy(mEmailWidget);
-    internetLayout->addWidget(mEmailWidget, 0, 1);
-
-    label = new QLabel(i18nc("@label The homepage URL of a contact", "Homepage:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    internetLayout->addWidget(label, 1, 0);
-
-    mHomepageWidget = new KLineEdit;
-    mHomepageWidget->setTrapReturnKey(true);
-    label->setBuddy(mHomepageWidget);
-    internetLayout->addWidget(mHomepageWidget, 1, 1);
-
-    label = new QLabel(i18nc("@label The blog URL of a contact", "Blog:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    internetLayout->addWidget(label, 2, 0);
-
-    mBlogWidget = new KLineEdit;
-    mBlogWidget->setTrapReturnKey(true);
-    label->setBuddy(mBlogWidget);
-    internetLayout->addWidget(mBlogWidget, 2, 1);
-
-    label = new QLabel(i18nc("@label The instant messaging address of a contact", "Messaging:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    internetLayout->addWidget(label, 3, 0);
-
-    mIMWidget = new IMEditWidget;
-    label->setBuddy(mIMWidget);
-    internetLayout->addWidget(mIMWidget, 3, 1);
-
-    internetLayout->setRowStretch(4, 1);
-
-    // setup phones group box
-    mPhonesWidget = new PhoneEditWidget;
-    phonesLayout->addWidget(mPhonesWidget, 0, 0);
-
-    //phonesLayout->setRowStretch( 1, 1 );
-
-    // setup categories section
-    QHBoxLayout *categoriesLayout = new QHBoxLayout;
-    label = new QLabel(i18nc("@label The categories of a contact", "Categories:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-
-    mCategoriesWidget = new CategoriesEditWidget;
-    label->setBuddy(mCategoriesWidget);
-
-    categoriesLayout->addWidget(label);
-    categoriesLayout->addWidget(mCategoriesWidget);
-
-    layout->addLayout(categoriesLayout, 1, 1);
-
-    QGroupBox *receivedMessageGroupBox = new QGroupBox(i18n("Messages"));
-    layout->addWidget(receivedMessageGroupBox, 2, 1);
-
-    QVBoxLayout *vbox = new QVBoxLayout(receivedMessageGroupBox);
-
-    QHBoxLayout *mailPreferFormattingLayout = new QHBoxLayout;
-    label = new QLabel(i18n("Show messages received from this contact as:"));
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    mMailPreferFormatting = new KComboBox;
-    label->setBuddy(mMailPreferFormatting);
-    QStringList listFormat;
-    listFormat << i18n("Default") << i18n("Plain Text") << i18n("HTML");
-    mMailPreferFormatting->addItems(listFormat);
-    mailPreferFormattingLayout->addWidget(label);
-    mailPreferFormattingLayout->addWidget(mMailPreferFormatting);
-
-    vbox->addLayout(mailPreferFormattingLayout);
-
-    mAllowRemoteContent = new QCheckBox(i18n("Allow remote content in received HTML messages"));
-    vbox->addWidget(mAllowRemoteContent);
-
-    layout->setRowStretch(4, 1);
+    mGeneralInfoWidget = new Akonadi::GeneralInfoWidget;
+    mTabWidget->addTab(mGeneralInfoWidget, i18nc("@title:tab", "Contact"));
 }
 
 void ContactEditorWidget::Private::initGuiLocationTab()
@@ -352,8 +210,9 @@ ContactEditorWidget::ContactEditorWidget(QWidget *parent)
     Q_UNUSED(parent)
     d->initGui();
 
-    connect(d->mNameWidget, &NameEditWidget::nameChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeName);
-    connect(d->mBusinessEditorWidget, &Akonadi::BusinessEditorWidget::organizationChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeOrganization);
+    //TODO PORT
+    //connect(d->mNameWidget, &NameEditWidget::nameChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeName);
+    //connect(d->mBusinessEditorWidget, &Akonadi::BusinessEditorWidget::organizationChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeOrganization);
 }
 
 ContactEditorWidget::ContactEditorWidget(ContactEditorWidget::DisplayMode displayMode, QWidget *parent)
@@ -362,8 +221,9 @@ ContactEditorWidget::ContactEditorWidget(ContactEditorWidget::DisplayMode displa
     Q_UNUSED(parent)
     d->initGui();
 
-    connect(d->mNameWidget, &NameEditWidget::nameChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeName);
-    connect(d->mBusinessEditorWidget, &Akonadi::BusinessEditorWidget::organizationChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeOrganization);
+    //TODO PORT
+    //connect(d->mNameWidget, &NameEditWidget::nameChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeName);
+    //connect(d->mBusinessEditorWidget, &Akonadi::BusinessEditorWidget::organizationChanged, d->mDisplayNameWidget, &DisplayNameEditWidget::changeOrganization);
 }
 
 ContactEditorWidget::~ContactEditorWidget()
@@ -372,38 +232,8 @@ ContactEditorWidget::~ContactEditorWidget()
 }
 
 void ContactEditorWidget::loadContact(const KContacts::Addressee &contact, const Akonadi::ContactMetaData &metaData)
-{
-    // name group
-    d->mPhotoWidget->loadContact(contact);
-    d->mNameWidget->loadContact(contact);
-    d->mDisplayNameWidget->loadContact(contact);
-    d->mNickNameWidget->setText(contact.nickName());
-
-    // Internet group
-    d->mEmailWidget->loadContact(contact);
-    d->mHomepageWidget->setText(contact.url().url().toString());
-    d->mBlogWidget->setText(d->loadCustom(contact, QStringLiteral("BlogFeed")));
-    d->mIMWidget->loadContact(contact);
-
-    // phones group
-    d->mPhonesWidget->loadContact(contact);
-
-    // categories section
-    d->mCategoriesWidget->loadContact(contact);
-
-    const QString mailPreferedFormatting = d->loadCustom(contact, QStringLiteral("MailPreferedFormatting"));
-    if (mailPreferedFormatting.isEmpty()) {
-        d->mMailPreferFormatting->setCurrentIndex(0);
-    } else if (mailPreferedFormatting == QLatin1String("TEXT")) {
-        d->mMailPreferFormatting->setCurrentIndex(1);
-    } else if (mailPreferedFormatting == QLatin1String("HTML")) {
-        d->mMailPreferFormatting->setCurrentIndex(2);
-    } else {
-        d->mMailPreferFormatting->setCurrentIndex(0);
-    }
-
-    const QString mailAllowToRemoteContent = d->loadCustom(contact, QStringLiteral("MailAllowToRemoteContent"));
-    d->mAllowRemoteContent->setChecked(mailAllowToRemoteContent == QLatin1String("TRUE"));
+{    
+    d->mGeneralInfoWidget->loadContact(contact);
 
     // address group
     d->mAddressesLocationWidget->loadContact(contact);
@@ -414,7 +244,6 @@ void ContactEditorWidget::loadContact(const KContacts::Addressee &contact, const
     d->mNotesWidget->setPlainText(contact.note());
 
     d->mPersonalEditorWidget->loadContact(contact);
-    d->mDisplayNameWidget->setDisplayType((DisplayNameEditWidget::DisplayType)metaData.displayNameMode());
 
     if (d->mDisplayMode == FullMode) {
         // custom fields group
@@ -430,42 +259,7 @@ void ContactEditorWidget::loadContact(const KContacts::Addressee &contact, const
 
 void ContactEditorWidget::storeContact(KContacts::Addressee &contact, Akonadi::ContactMetaData &metaData) const
 {
-    // name group
-    d->mPhotoWidget->storeContact(contact);
-    d->mNameWidget->storeContact(contact);
-    d->mDisplayNameWidget->storeContact(contact);
-    contact.setNickName(d->mNickNameWidget->text().trimmed());
-
-    // Internet group
-    d->mEmailWidget->storeContact(contact);
-    KContacts::ResourceLocatorUrl resourceLocatorUrl;
-    resourceLocatorUrl.setUrl(QUrl(d->mHomepageWidget->text().trimmed()));
-    contact.setUrl(resourceLocatorUrl);
-    d->storeCustom(contact, QStringLiteral("BlogFeed"), d->mBlogWidget->text().trimmed());
-    d->mIMWidget->storeContact(contact);
-
-    // phones group
-    d->mPhonesWidget->storeContact(contact);
-
-    // categories section
-    d->mCategoriesWidget->storeContact(contact);
-
-    QString mailPreferedFormatting;
-    const int index = d->mMailPreferFormatting->currentIndex();
-    if (index == 0) {
-        //Nothing => remove custom variable
-    } else if (index == 1) {
-        mailPreferedFormatting = QStringLiteral("TEXT");
-    } else if (index == 2) {
-        mailPreferedFormatting = QStringLiteral("HTML");
-    }
-    d->storeCustom(contact, QStringLiteral("MailPreferedFormatting"), mailPreferedFormatting);
-
-    QString mailAllowToRemoteContent;
-    if (d->mAllowRemoteContent->isChecked()) {
-        mailAllowToRemoteContent = QStringLiteral("TRUE");
-    }
-    d->storeCustom(contact, QStringLiteral("MailAllowToRemoteContent"), mailAllowToRemoteContent);
+    d->mGeneralInfoWidget->storeContact(contact);
 
     // address group
     d->mAddressesLocationWidget->storeContact(contact);
@@ -482,7 +276,7 @@ void ContactEditorWidget::storeContact(KContacts::Addressee &contact, Akonadi::C
         d->mCustomFieldsWidget->storeContact(contact);
         metaData.setCustomFieldDescriptions(d->mCustomFieldsWidget->localCustomFieldDescriptions());
 
-        metaData.setDisplayNameMode(d->mDisplayNameWidget->displayType());
+        //TODO metaData.setDisplayNameMode(d->mDisplayNameWidget->displayType());
 
         // custom pages
         foreach (Akonadi::ContactEditorPagePlugin *plugin, d->mCustomPages) {
@@ -493,28 +287,7 @@ void ContactEditorWidget::storeContact(KContacts::Addressee &contact, Akonadi::C
 
 void ContactEditorWidget::setReadOnly(bool readOnly)
 {
-    // widgets from name group
-    d->mNameWidget->setReadOnly(readOnly);
-    d->mPhotoWidget->setReadOnly(readOnly);
-    d->mDisplayNameWidget->setReadOnly(readOnly);
-    d->mNickNameWidget->setReadOnly(readOnly);
-
-    // widgets from Internet group
-    d->mEmailWidget->setReadOnly(readOnly);
-    d->mHomepageWidget->setReadOnly(readOnly);
-    d->mBlogWidget->setReadOnly(readOnly);
-    d->mIMWidget->setReadOnly(readOnly);
-
-    // widgets from phones group
-    d->mPhonesWidget->setReadOnly(readOnly);
-
-    // widgets from categories section
-    d->mCategoriesWidget->setReadOnly(readOnly);
-
-    // Preferred Mail formatting option
-    d->mMailPreferFormatting->setEnabled(!readOnly);
-    d->mAllowRemoteContent->setEnabled(!readOnly);
-
+    d->mGeneralInfoWidget->setReadOnly(readOnly);
     // widgets from addresses group
     d->mAddressesLocationWidget->setReadOnly(readOnly);
 
