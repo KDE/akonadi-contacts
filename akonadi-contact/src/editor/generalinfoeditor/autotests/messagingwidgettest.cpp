d5a7dde5e860434d2dc9df83110f5d681ac08ced
Montel Laurent <montel@kde.org>
2016-02-15 13:49:19 +0100

    implement autotest

--- akonadi-contact/src/editor/generalinfoeditor/autotests/messagingwidgettest.cpp
+++ akonadi-contact/src/editor/generalinfoeditor/autotests/messagingwidgettest.cpp
@@ -22,7 +22,10 @@
 
 #include "messagingwidgettest.h"
 #include "../messaging/messagingwidget.h"
+#include <KLineEdit>
+#include <QComboBox>
 #include <QTest>
+#include <QToolButton>
 
 MessagingWidgetTest::MessagingWidgetTest(QObject *parent)
     : QObject(parent)
@@ -38,7 +41,17 @@ MessagingWidgetTest::~MessagingWidgetTest()
 void MessagingWidgetTest::shouldHaveDefaultValues()
 {
     Akonadi::MessagingWidget w;
+    KLineEdit *line = w.findChild<KLineEdit *>(QStringLiteral("messaginglineedit"));
+    QVERIFY(line);
 
+    QComboBox *protocolCombo = w.findChild<QComboBox *>(QStringLiteral("protocol"));
+    QVERIFY(protocolCombo);
+    QVERIFY(protocolCombo->count() > 0);
+
+    QToolButton *add = w.findChild<QToolButton *>(QStringLiteral("addbutton"));
+    QVERIFY(add);
+    QToolButton *remove= w.findChild<QToolButton *>(QStringLiteral("removebutton"));
+    QVERIFY(remove);
 }
 
 QTEST_MAIN(MessagingWidgetTest)
