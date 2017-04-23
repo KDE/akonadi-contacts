b819a55d923db781f44dd1eba52a168db9bece45
Montel Laurent <montel@kde.org>
2017-04-23 08:55:24 +0200

    Exclude autotest/test

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui -o -iname \*.kcfg -o -iname \*.kcfg.cmake` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.h -o -iname \*.cpp` -o $podir/akonadicontact5.pot
+$XGETTEXT `find . -name \*.h -o -iname \*.cpp  | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/akonadicontact5.pot
