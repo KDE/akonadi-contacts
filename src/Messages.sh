f7fe29ae7ce963922dfa832d3de1a7884d52fb6e
Montel Laurent <montel@kde.org>
2017-04-23 08:53:15 +0200

    Not necessary

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui -o -iname \*.kcfg -o -iname \*.kcfg.cmake` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.h -o -iname \*.cpp` -o $podir/akonadicontact5.pot
-rm -f rc.cpp
