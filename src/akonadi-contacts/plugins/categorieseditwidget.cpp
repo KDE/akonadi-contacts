81dfcc24e50eb810af1f3bed42d5bdffa9dc60f8
Laurent Montel <montel@kde.org>
2018-03-11 17:34:30 +0100

    GIT_SILENT: Prepare 5.8 beta1

--- src/akonadi-contacts/plugins/categorieseditwidget.cpp
+++ src/akonadi-contacts/plugins/categorieseditwidget.cpp
@@ -29,9 +29,6 @@
 #include <QHBoxLayout>
 #include <kcoreaddons_version.h>
 #include <kpluginfactory.h>
-#if KCOREADDONS_VERSION < QT_VERSION_CHECK(5, 44, 0)
-#define K_PLUGIN_CLASS_WITH_JSON(classname, json) K_PLUGIN_FACTORY_WITH_JSON(classname ## Factory, json, registerPlugin<classname >();)
-#endif
 
 K_PLUGIN_CLASS_WITH_JSON(CategoriesEditWidget, "categorieseditwidgetplugin.json")
 
