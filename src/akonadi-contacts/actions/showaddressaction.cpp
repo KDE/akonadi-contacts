2af01e5c97c186c43985d18ca96fdbfa467ceda2
Volker Krause <vkrause@kde.org>
2021-10-25 17:51:27 +0200

    Port KContacts::Address::countryToISO to new KI18n API
    This function in KContacts is about to be deprecated.

--- src/akonadi-contacts/actions/showaddressaction.cpp
+++ src/akonadi-contacts/actions/showaddressaction.cpp
@@ -13,6 +13,10 @@
 #include <KContacts/Address>
 #include <KDialogJobUiDelegate>
 #include <KIO/CommandLauncherJob>
+#include <ki18n_version.h>
+#if KI18N_VERSION >= QT_VERSION_CHECK(5, 88, 0)
+#include <KCountry>
+#endif
 
 #include <QDesktopServices>
 
@@ -20,12 +24,22 @@ using namespace Akonadi;
 
 static void replaceArguments(QString &templateStr, const KContacts::Address &address)
 {
+#if KI18N_VERSION >= QT_VERSION_CHECK(5, 88, 0)
+    const auto country = KCountry::fromName(address.country()); // will also handle ISO 3166-1 alpha 2 or 3 codes
+    templateStr.replace(QLatin1String("%s"), address.street())
+        .replace(QLatin1String("%r"), address.region())
+        .replace(QLatin1String("%l"), address.locality())
+        .replace(QLatin1String("%z"), address.postalCode())
+        .replace(QLatin1String("%n"), country.name())
+        .replace(QLatin1String("%c"), country.alpha2());
+#else
     templateStr.replace(QLatin1String("%s"), address.street())
         .replace(QLatin1String("%r"), address.region())
         .replace(QLatin1String("%l"), address.locality())
         .replace(QLatin1String("%z"), address.postalCode())
         .replace(QLatin1String("%n"), address.country())
         .replace(QLatin1String("%c"), address.countryToISO(address.country()));
+#endif
 }
 
 void ShowAddressAction::showAddress(const KContacts::Address &address)
