369c1db7da1a225e3a34e3d66bca2c6b2d5f645b
Laurent Montel <montel@kde.org>
2022-03-29 07:45:06 +0200

    Adapt include for 5.93

--- src/akonadi-contacts/contactgroupeditor.cpp
+++ src/akonadi-contacts/contactgroupeditor.cpp
@@ -8,6 +8,10 @@
 
 #include "contactgroupeditor.h"
 #include "contactgroupeditor_p.h"
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush> // was moved to own header in 5.93.0
+#endif
 
 #include "contactgroupeditordelegate_p.h"
 #include "contactgroupmodel_p.h"
