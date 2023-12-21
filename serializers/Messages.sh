6ed0713f15aa7748d80b54090a5ef7ddc8d62c66
Alessandro Astone <ales.astone@gmail.com>
2023-12-21 14:38:46 +0100

    Fix the translation template version number (5->6)
    While this module does not need to be co-installable with its Qt5 version,
    change the version suffix for consistency.

--- serializers/Messages.sh
+++ serializers/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$XGETTEXT `find . -name \*.cpp | grep -v '/autotests/'` -o $podir/akonadicontact5-serializer.pot
+$XGETTEXT `find . -name \*.cpp | grep -v '/autotests/'` -o $podir/akonadicontact6-serializer.pot
 
 
--- src/Messages.sh
+++ src/Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name \*.ui -o -iname \*.kcfg -o -iname \*.kcfg.cmake` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.h -o -iname \*.cpp  | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/akonadicontact5.pot
+$XGETTEXT `find . -name \*.h -o -iname \*.cpp  | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/akonadicontact6.pot
