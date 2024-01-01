d40d38b0e36ada01b678e0fd85da862792ee5325
Laurent Montel <montel@kde.org>
2024-01-01 12:26:34 +0100

    GIT_SILENT: happy new year

--- src/akonadi-contacts-widgets/editor/generalinfoeditor/autotests/blogfeedwidgettest.cpp
+++ src/akonadi-contacts-widgets/editor/generalinfoeditor/autotests/blogfeedwidgettest.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of Contact Editor.
 
-    SPDX-FileCopyrightText: 2018-2023 Laurent Montel <montel@kde.org>
+    SPDX-FileCopyrightText: 2018-2024 Laurent Montel <montel@kde.org>
 
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
