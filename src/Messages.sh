3a6115e5247b4d8aa40345a7b51603470506a032
Laurent Montel <montel@kde.org>
2024-03-22 07:14:30 +0100

    We don't have kcfg* file

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
-$EXTRACTRC `find . -name \*.ui -o -iname \*.kcfg -o -iname \*.kcfg.cmake` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.h -o -iname \*.cpp  | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/akonadicontact6.pot
