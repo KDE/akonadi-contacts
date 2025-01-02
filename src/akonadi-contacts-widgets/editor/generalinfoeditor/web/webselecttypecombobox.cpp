90e9cac6280d1843e5820d9b0ecdbb21131ce3c9
Laurent Montel <montel@kde.org>
2025-01-02 06:58:14 +0100

    GIT_SILENT: happy new year

--- src/akonadi-contacts-widgets/editor/generalinfoeditor/web/webselecttypecombobox.cpp
+++ src/akonadi-contacts-widgets/editor/generalinfoeditor/web/webselecttypecombobox.cpp
@@ -1,7 +1,7 @@
 /*
     This file is part of Contact Editor.
 
-    SPDX-FileCopyrightText: 2019-2024 Laurent Montel <montel@kde.org>
+    SPDX-FileCopyrightText: 2019-2025 Laurent Montel <montel@kde.org>
 
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
