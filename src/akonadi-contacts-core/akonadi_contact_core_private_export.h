f81a5669a81e87e34c8d1c9f47dfda62cc67ec7e
Laurent Montel <montel@kde.org>
2026-01-06 13:34:13 +0100

    Add private export include

---
+++ src/akonadi-contacts-core/akonadi_contact_core_private_export.h
@@ -0,0 +1,18 @@
+/*  This file is part of the KDE project
+    SPDX-FileCopyrightText: 2017-2026 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "akonadi-contact-core_export.h"
+
+/* Classes which are exported only for unit tests */
+#ifdef BUILD_TESTING
+#ifndef AKONADI_CONTACT_CORE_TESTS_EXPORT
+#define AKONADI_CONTACT_CORE_TESTS_EXPORT AKONADI_CONTACT_CORE_EXPORT
+#endif
+#else /* not compiling tests */
+#define AKONADI_CONTACT_CORE_TESTS_EXPORT
+#endif
